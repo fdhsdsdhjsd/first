0b63809e860aab8e4e327939f431fab7c34042af
fdhsdsdhjsd <134865024+fdhsdsdhjsd@users.noreply.github.com>
2023-05-30 12:33:39 +0300

    Create updateText.js

---
+++ updateText.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var element = document.querySelector('#elNavSecondary_45 a');
+  if (element) {
+    element.textContent = 'Upgrade Account';
+  }
+});
