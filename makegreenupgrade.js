d10bf0d96a96f240cdede1f583198c420b47bef8
fdhsdsdhjsd <134865024+fdhsdsdhjsd@users.noreply.github.com>
2023-05-31 20:59:58 +0300

    Update and rename test1.js to makegreenupgrade.js

---
+++ makegreenupgrade.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var currentUrl = window.location.href;
+    var upgradeWord = "upgrade";
+
+    if (currentUrl.includes(upgradeWord)) {
+      var element = document.querySelector('[data-navitem-id="47"]');
+      if (element) {
+        element.style.color = "#59ff00";
+      }
+    }
+  });
